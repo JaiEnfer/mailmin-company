ecaf0c3f22478b8efb93b4157b2d00777524d3fa
JaiEnfer <prakashjai990@gmail.com>
2026-03-05 00:55:32 +0100

    Remove revision annotation for Alembic

--- backend/alembic/versions/3225c43b3b3b_add_google_email_to_workspace.py
+++ backend/alembic/versions/3225c43b3b3b_add_google_email_to_workspace.py
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import inspect
 
-revision: str = "3225c43b3b3b"
+revision = "3225c43b3b3b"
 down_revision = "5d6c26d92e0a"
 branch_labels = None
 depends_on = None
