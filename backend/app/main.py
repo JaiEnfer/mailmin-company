f6e1654e4fcf0a22e3a8111db93b60119c1ad0c1
JaiEnfer <prakashjai990@gmail.com>
2026-03-05 13:30:06 +0100

    Fixing the connection for frontend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="MailMind API", version="1.0.0")
 frontend = os.getenv("FRONTEND_URL", "http://localhost:3000")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[frontend, "http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
